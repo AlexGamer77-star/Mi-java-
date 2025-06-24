1c1d77cef4b9491d85d1ab7fd9eaa5e21d486a7b
Mauricioreyes <mauricio.reyes036@std.uni.edu.ni>
2025-06-24 17:58:09 -0600

    codigo de la suma de a + b

---
+++ src/Holamundo.java
@@ -0,0 +1,10 @@
+public class Holamundo {
+    public static void main(String[] args) throws Exception {
+        String Nombre = "Estudiante";
+        int a=5;
+        int b=3;
+        int suma= a + b;
+        System.out.println(" Hola "+ Nombre );
+        System.out.println(" La suma de "+ a + " + "+ b +" es: "+ suma);
+    }
+}
