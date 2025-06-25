5ba0c39d902cb9ed74f095027893d417640f2da4
Mauricioreyes <mauricio.reyes036@std.uni.edu.ni>
2025-06-24 20:39:20 -0600

    cambio saludo

--- src/Holamundo.java
+++ src/Holamundo.java
@@ -1,7 +1,8 @@
 public class Holamundo {
     public static void main(String[] args) throws Exception {
+        System.out.println("Mauricio Alexander Reyes Rivera 2025-3394u");
         String Nombre = "Estudiante";
-        int a=5;
+        int a=7; 
         int b=3;
         int mult= a * b;
         System.out.println(" Hola "+ Nombre );
